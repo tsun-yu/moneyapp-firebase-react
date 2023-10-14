9413191a40396a676157786a17df1b2da82b2fa8
Wayne <tsun0227@yahoo.com.tw>
2023-10-14 15:27:01 +0800

    feat: logout

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Hamburger from "./Hamburger";
 import { TbBrandFirebase } from "react-icons/tb";
+import { BsPersonCircle } from "react-icons/bs";
+import { signOut } from "firebase/auth";
+import { auth } from "src/util/firebase";
 
 interface HeaderProps {
   isChecked: boolean;
@@ -13,25 +16,44 @@ const Container = styled.header`
   background-color: var(--bg-secondary);
   display: flex;
   align-items: center;
+  justify-content: space-between;
   grid-area: header;
   padding-left: 1.125rem;
 
-  .header__logo {
-    font-size: 1.5rem;
-    color: #555;
+  div {
     display: flex;
     align-items: center;
+
+    .header__logo {
+      font-size: 1.5rem;
+      color: #555;
+      display: flex;
+      align-items: center;
+    }
+  }
+  .header__member {
+    font-size: 1.5rem;
+    color: #555;
+    cursor: pointer;
   }
 `;
 
 function Header(props: HeaderProps) {
+  const logOut = async () => {
+    await signOut(auth);
+  };
   return (
     <Container>
-      <Hamburger {...props} />
-      <h1 className="header__logo">
-        <TbBrandFirebase />
-        Firebase - React
-      </h1>
+      <div>
+        <Hamburger {...props} />
+        <h1 className="header__logo">
+          <TbBrandFirebase />
+          Firebase - React
+        </h1>
+      </div>
+      <div className="header__member" onClick={logOut}>
+        <BsPersonCircle />
+      </div>
     </Container>
   );
 }
