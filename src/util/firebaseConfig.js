c747382c5a56032d8e0d00adaab8f0d1fa5638f3
Wayne <tsun0227@yahoo.com.tw>
2023-10-14 15:13:17 +0800

    feat: .env

--- src/util/firebaseConfig.js
+++ src/util/firebaseConfig.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD6fiBvFHbgrWGrGW0gD044IDlyy7aloKo",
-  authDomain: "crud-testing-c6f10.firebaseapp.com",
-  projectId: "crud-testing-c6f10",
-  storageBucket: "crud-testing-c6f10.appspot.com",
-  messagingSenderId: "649401055856",
-  appId: "1:649401055856:web:6ba3018a9b2db04b2112b5",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
